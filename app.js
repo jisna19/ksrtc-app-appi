946ca0a1af943dd66f16286d1db426d39292e015
jisna19 <jisnavarghese99@gmail.com>
2024-06-14 06:39:11 +0530

    completed

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express=require("express")
 const mongoose=require("mongoose")
 const cors=require("cors")
+const bcrypt=require("bcryptjs")//for cypher
 const {usermodel}=require("./models/signup")
 
 const app=express()
@@ -8,14 +9,20 @@ app.use(cors())
 app.use(express.json())
 mongoose.connect("mongodb+srv://jisna19:jisna9947@cluster0.fzxusrj.mongodb.net/busDB?retryWrites=true&w=majority&appName=Cluster0")
 
+const generateHashedPassword=async(password)=>{
+    const salt=await bcrypt.genSalt(10)
+    return bcrypt.hash(password,salt)
 
+}
 
-app.post("/signup",(req,res)=>{
+app.post("/signup",async (req,res)=>{
     let input=req.body
-    let user=new usermodel(input)
-    user.save()
-    console.log(user)
-    res.send({"status":"success"})
+    let hashesdPassword=await generateHashedPassword(input.password)
+    console.log(hashesdPassword)
+    input.password=hashesdPassword
+    let signup=new usermodel(input)
+    signup.save()
+    res.json({"status":"success"})
 })
 
 
